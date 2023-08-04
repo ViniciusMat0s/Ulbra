4deed5c2083aa50dad914f529df3879fb9ded2dc
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2023-08-04 01:26:35 -0300

    2sem

---
+++ Ulbra 2023-2/Laboratório de Programação/Aula 02 (Curso Bradesco e projeto)/Conclusão1 (Média de alunos)/Curso/Program.cs
@@ -0,0 +1,33 @@
+﻿class Conclusao
+{
+    static void Main()
+    {
+        float media = 0;
+
+        Console.WriteLine("Olá, seja bem-vindo ao programa de cálculo de média!");
+
+        Console.WriteLine("Irei auxiliá-lo a calcular a média de seu aluno!");
+
+        Console.WriteLine("Digite a nota da AP1: ");
+        int.TryParse(Console.ReadLine(), out int nota1);
+
+        Console.WriteLine("Digite a nota da AP2: ");
+        int.TryParse(Console.ReadLine(), out int nota2);
+
+        Console.WriteLine("Digite a nota da AS: ");
+        int.TryParse(Console.ReadLine(), out int nota3);
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+        Console.WriteLine("A média do aluno é: {0}", media);
+
+        if (media < 7)
+        {
+            Console.WriteLine("O aluno deve realizar a AF.");
+        }
+        else
+        {
+            Console.WriteLine("Aluno aprovado.");
+        }
+    }
+}
---
+++ Ulbra 2023-2/Laboratório de Programação/Aula 02 (Curso Bradesco e projeto)/Conclusão2 (Calculadora de soma)/Conclusão2/Program.cs
@@ -0,0 +1,36 @@
+﻿class Conclusao2
+{
+    static void Main()
+    {
+        bool opcao = true;
+
+        Console.WriteLine("Olá, irei realizar a soma dos números que você deseja até onde você quiser");
+
+        Console.WriteLine("Digite o valor inicial: ");
+        float.TryParse(Console.ReadLine(), out float inicial);
+
+        while(opcao == true)
+        {
+            Console.WriteLine("Digite o número que você deseja somar ao número inicial: ");
+            float.TryParse(Console.ReadLine(), out float n1);
+            inicial = inicial + n1;
+            Console.WriteLine("A soma até o momento é: {0}", inicial);
+            Console.WriteLine("Digite [1] para sair do programa ou [2] para continuar: ");
+            int.TryParse(Console.ReadLine(), out int saida);
+            switch (saida)
+            {
+                case 1:
+                    Console.WriteLine("Saindo do Programa.");
+                    opcao = false;
+                    break;
+                case 2:
+                    Console.WriteLine("Continuando a soma.");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida, retornando à soma.");
+                    break;
+
+            }
+        }
+    }
+}
